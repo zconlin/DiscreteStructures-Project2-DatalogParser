f8fea680b8ab13256c7143570a067491046fbc62
jcc01 <jcc01@byu.edu>
2023-02-14 16:27:19 -0700

    Hints

--- parser.h
+++ parser.h
@@ -27,18 +27,20 @@ private:
     DatalogProgram dp;
 
 public:
-    string toString() {
-        stringstream output;
-        int numTokens = 0;
-        output << tokenString() << " (" << numTokens << ") :" << endl;
-
-        return output.str();
-    }
+    //// You're on the right track, but I wouldn't implement it in this function. Your toString should be self-contained in the DatalogProgram class for later projects
+//    string toString() {
+//        stringstream output;
+//        int numTokens = 0;
+//        output << tokenString() << " (" << numTokens << ") :" << endl;
+//
+//        return output.str();
+//    }
 
     Parser(const vector<Token>& tokens) : tokens(tokens) {
-        if(tokenType() == COMMENT) {
-            advanceToken();
-        }
+        //// I don't think you need this constructor. You're going to call a parse function later.
+//        if(tokenType() == COMMENT) {
+//            advanceToken();
+//        }
     }
 
     TokenType tokenType() const { // Returns the type of the current Token
@@ -55,19 +57,22 @@ public:
         }
     }
 
+    //// I made my throwError function follow the signature "void throwError(Token t)" and threw the token, made it easy to print the offending token at the end
     void throwError() { // Is called when the Parser finds an error
 //        throw "Error";
 //cout << "Error"; //debug
     }
 
-    int c = 0;
-    void match(TokenType t) {
+    int c = 0; //// I think this probably can get removed...
+    void match(TokenType t) { //// return the Token in the match function
         if (tokens.at(c).getType() == END) {
 
         }
         if (tokens.at(c).getType() == t) {
             cout << tokens.at(c).getValue(); //debug
+            //// save the token before you advance and delete it
             advanceToken();
+            //// return the saved token
         }
     }
 
@@ -80,29 +85,31 @@ public:
     DatalogProgram datalogProgram () {
             match(SCHEMES);
             match(COLON);
-            scheme();
-            schemeList();
+            scheme(); //// Each of these functions should get a value saved.
+            schemeList(); //// ^^
             match(FACTS);
             match(COLON);
-            factList();
+            factList(); //// ^^
             match(RULES);
             match(COLON);
-            ruleList();
+            ruleList(); //// ^^
             match(QUERIES);
             match(COLON);
-            query();
-            queryList();
+            query(); //// ^^
+            queryList(); //// ^^
             match(END);
-            return {}; //TODO return real datalog program
+            return {}; //// return the DatalogProgram Object returned by the DatalogProgram() constructor
     }
 
     //// schemeList	->	scheme schemeList | lambda
-    void schemeList() {
+    void schemeList() { //// Shouldn't be void but should return vector<Predicate> (a list of all the schemes parsed)
         if(tokenType() == ID) {
-            scheme();
-            schemeList();
+            scheme(); //// Each of these functions should get a value saved.
+            schemeList(); //// Each of these functions should get a value saved.
+            //// return the list of schemes saved out from the parsing
         } else {
             // lambda
+            //// return {} (an empty schemes list)
         }
     }
 
@@ -118,7 +125,7 @@ public:
 
     //// ruleList	->	rule ruleList | lambda
     void ruleList() {
-        if(tokenType() != QUERIES) {
+        if(tokenType() != QUERIES) { //// I don't think this should be QUERIES... What is the next token when you're in the rules section
             rule();
             ruleList();
         } else {
@@ -138,30 +145,32 @@ public:
 
     //// SCHEME     ----------------------------------------------------------------------------------
     //// scheme   	-> 	ID LEFT_PAREN ID idList RIGHT_PAREN
-     void scheme() {
+     void scheme() { //// return a scheme (Predicate)
         if (tokenType() == ID) {
-            match(ID);
+            match(ID); //// Save the token! don't just throw it away
             match(LEFT_PAREN);
-            match(ID);
-            idList();
+            match(ID); //// Save the token
+            idList(); //// Save the list of Ids to a variable
             match(RIGHT_PAREN);
+            //// return Predicate(NAMETOKEN.value, IDLISTVAR)
         }
     }
 
     //// FACT     ----------------------------------------------------------------------------------
     //// fact    	->	ID LEFT_PAREN STRING stringList RIGHT_PAREN PERIOD
-    void fact() {
+    void fact() { //// return a fact (Predicate)
         if (tokenType() == ID) {
-            match(ID);
+            match(ID); //// save
             match(LEFT_PAREN);
-            match(STRING);
+            match(STRING); //// save
             // string value to string vector? Not quite but close
-            stringList();
+            stringList(); //// save
             match(RIGHT_PAREN);
             match(PERIOD);
             // push to datalogProgram fact vector
 //            dp.Facts.push_back();
 //            Predicate().addParameter();
+            //// Return Predicate(name, strings)
         }
     }
 
